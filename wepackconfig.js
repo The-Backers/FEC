4709838824a6b753441230a997df37f5bd65d7a4
linlinlin3 <linnxh@gmail.com>
2021-02-23 19:34:19 -0500

    set up webpack and create folder

---
+++ wepackconfig.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
