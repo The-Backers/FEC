3daf8905b3ccf82b556ebc3036d5839d20a9263c
David <dbdavidbacker@gmail.com>
2021-02-24 19:08:45 -0500

    Heres a sample api request i guess

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import API_KEY from '../../config.js'
 
 class App extends React.Component {
   constructor(props) {
@@ -8,9 +10,24 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-nyc/reviews?product_id=2',
+      headers: {'Authorization': API_KEY}
+    })
+    .then((result) => {this.setState({value: result.data})})
+    .catch(this.setState({value: "It didn't work, sad"}))
+  }
+
   render() {
     return (
-      <div>Hello World!</div>
+      <div>
+        <h1>API Test</h1>
+          <p>{JSON.stringify(this.state.value)}</p>
+      </div>
+
+
     );
   }
 }
