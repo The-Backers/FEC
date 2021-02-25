9170866995173ddc52b49d919cf2504958f06fc0
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-02-25 15:00:10 -0500

    Card #33: created basic file structure system

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,6 @@
 // this is where Tests for App.jsx go!
 const React = require('react');
 const enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-enzyme.configure({ adapter: new Adapter() });
 
 const App = require('./App').default;
 
@@ -12,6 +9,6 @@ describe('App', () => {
   test('should render App with correct text', () => {
     const wrapper = enzyme.shallow(<App />);
     expect(wrapper.exists()).toBeTruthy();
-    expect(wrapper.text() === 'Hello World!').toBeTruthy;
+    expect(wrapper.text() === 'Hello World!').not.toBeTruthy();
   });
 });
--- testing/example.test.js
+++ testing/example.test.js
@@ -2,6 +2,9 @@
 const React = require('react');
 const enzyme = require('enzyme');
 
+// importing a component would look like this:
+// const App = require('../src/components/App').default;
+
 // example component
 class App extends React.Component {
   constructor(props) {
@@ -13,23 +16,20 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World!</div>
+      <div id="test">Hello World!</div>
     );
   }
 }
 
-// importing a component would look like this:
-// const App = require('../src/components/App').default;
-
-// example test using shallow rendering (for testing components by themselves, https://enzymejs.github.io/enzyme/docs/api/shallow.html)
 describe('App', () => {
+  // example test using shallow rendering (for testing components by themselves, https://enzymejs.github.io/enzyme/docs/api/shallow.html)
   test('should render App with correct text', () => {
     const wrapper = enzyme.shallow(<App />);
-    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('#test')).toBeTruthy();
     expect(wrapper.text()).toBe('Hello World!');
   });
+  // example test using mount (mounts component to the DOM, https://enzymejs.github.io/enzyme/docs/api/mount.html)
+
+  // example test using render (generates HTML and tests for structure, https://enzymejs.github.io/enzyme/docs/api/render.html)
+
 });
-
-// example test using mount (mounts component to the DOM, https://enzymejs.github.io/enzyme/docs/api/mount.html)
-
-// example test using render (generates HTML and tests for structure, https://enzymejs.github.io/enzyme/docs/api/render.html)
