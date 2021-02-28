276c1243c5e96c0e96f554ea159110a5e9e82f7f
amenchaca100 <menchacanton@gmail.com>
2021-02-28 17:57:06 -0500

    create basic nav-bar no functionality

--- src/components/overview/index.js
+++ src/components/overview/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Overview = ({product, demoButton}) =>(
   <div className="parent-overview">
 <nav className='nav'>
-  <h1>Nav bar will go here</h1>
+  <h1>Logo</h1>
+  <div className='search-container'>
+  <input type='text' className='prod-search'></input>
+  <i className="fas fa-search"></i>
+  </div>
+
 </nav>
 <div className='overview-body'>
   <div className='overview-body-image'>
