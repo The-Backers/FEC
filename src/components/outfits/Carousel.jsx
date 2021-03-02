863f30afdd2297727d28e238599f75de5ab29ea1
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-01 22:04:06 -0500

    card #42: rendered products to page on first render, modularized relatedItems component

--- src/components/outfits/Carousel.jsx
+++ src/components/outfits/Carousel.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const Carousel = () => {
-  return (
-    <div>Hello this is carousel!</div>
-  );
+const Carousel = ({relatedProducts}) => {
+  if (relatedProducts.length > 0) {
+    return (
+      <div>
+        {relatedProducts.map((product, index) => {
+          return (
+            <div key={index}>
+              <h4>{product.name}</h4>
+              <div>Image?</div>
+              <span>{product.category}</span>
+              <span>: ${product.default_price}</span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 export default Carousel;
-
-/*
-Add AUTHORIZATION: API_KEY header to each axios request
-axios request to: https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/{product_id}/related
-and then to each product related: https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/{product_id}
-*/
--- src/components/outfits/index.js
+++ src/components/outfits/index.js
@@ -1,35 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from './Carousel.jsx';
 import ProductCard from './ProductCard.jsx';
 import Comparison from './Comparison.jsx';
 
 const RelatedItems = ({fetchRelated, relatedProducts}) => {
-  console.log('this is relatedProducts: ', relatedProducts);
-  if (relatedProducts.length > 0) {
-    return (
-      <div className="parent-products">
-        {relatedProducts.map((product, index) => {
-          return (
-            <div key={index}>
-              <h4>{product.name}</h4>
-              <div>Image?</div>
-              <span>{product.category}</span>
-              <span>: ${product.default_price}</span>
-            </div>
-          )
-        })}
-      </div>
-    )
-  } else {
-    return (
-      <div className="parent-products">
-        <button onClick={fetchRelated}>Fetch Related!</button>
-        <Carousel className="products-related"/>
-        <ProductCard className="products-outfits"/>
-        <Comparison className="comparison-card" />
-      </div>
-    )
-  }
+
+  useEffect(() => {
+    fetchRelated();
+  }, []);
+
+  return (
+    <div className="parent-products">
+      <Carousel className="products-related" relatedProducts={relatedProducts}/>
+      <Comparison className="comparison-card" />
+    </div>
+  )
 };
 
 export default RelatedItems;
