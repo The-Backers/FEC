7346bc3730e7dca006147daa6d93e9abddca8b6b
David <dbdavidbacker@gmail.com>
2021-03-02 15:55:13 -0500

    Show more button works

--- src/components/reviews/ReviewTile.jsx
+++ src/components/reviews/ReviewTile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//assigns month names to numbers
 var months = {
   0: 'January',
   1: 'February',
@@ -16,23 +17,73 @@ var months = {
 }
 
 
-const ReviewTile = ({review}) => {
+class ReviewTile extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      day: this.props.review.date.substring(8, 10),
+      month: parseInt(this.props.review.date.substring(5,7)),
+      year: this.props.review.date.substring(0, 4),
+      body1: this.props.review.body.substring(0, 250),
+      body2: this.props.review.body.substring(250, this.props.review.body.length)
+    }
+    this.more = React.createRef();
+    this.showMore = this.showMore.bind(this);
+  }
+
+//sets the string for the date itself
+
+
+showMore(event) {
+
+    if (this.more.current.style.display !== 'none') {
+      this.more.current.style.display = 'none';
+      var x = this.state.body1 + '...';
+      event.target.innerHTML = 'Show More';
+      this.setState({body1: x});
+    } else {
+      this.more.current.style.display = 'inline';
+      event.target.innerHTML = 'Show Less';
+      this.setState({body1: this.state.body1.substring(0, this.state.body1.length - 3)})
+    }
+
+}
+
+componentDidMount() {
+    this.more.current.style.display = 'none'
+  if (this.state.body2.length > 0) {
+    var x = this.state.body1 + '...';
+    this.setState({body1: x});
+  }
+}
+
+render() {
 
 
 
-  var year = review.date.substring(0, 4)
-  var month = parseInt(review.date.substring(5,7))
-  var day = review.date.substring(8, 10)
+  var date = `${months[this.state.month]} ${this.state.day}, ${this.state.year}`
 
-  var date = `${months[month]} ${day}, ${year}`
 
   return (
-    <div>
-      <h3>{review.summary}</h3>
-      <p>{date}</p>
-      <p>{review.body}</p>
+    <div className = 'review-tile'>
+      <h3 className = 'review-summary'>{this.props.review.summary}</h3>
+      <p className = 'review-date'>{date}</p>
+      <p>{this.state.body1}
+
+          <span className = 'review-body-more' ref = {this.more}>{this.state.body2}</span>
+      </p>
+      {
+      (() => {
+        if (this.state.body2.length > 0) {
+        return (<button onClick = {this.showMore}>Show More</button>)
+          }
+        })()
+      }
     </div>
   )
+}
+
 }
 
 
