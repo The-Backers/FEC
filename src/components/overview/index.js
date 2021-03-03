c94cc1c1fc422c4bbe6cc895723b49485226ca66
amenchaca100 <menchacanton@gmail.com>
2021-03-03 14:16:49 -0500

    grabbing changes from master

--- src/components/overview/index.js
+++ src/components/overview/index.js
@@ -4,28 +4,26 @@ import ExpandedGallery from './ExpandedGallery.js';
 import DefaultGallery from './DefaultGallery.js';
 
 const Overview = () => {
-
   let Gallery = () => {
     const [expand, setExpand] = useState(false);
-
     if (expand) {
       return <ExpandedGallery expand={expand} setExpand={setExpand}/>
     } else {
       return <DefaultGallery expand={expand} setExpand={setExpand}/>
     }
   }
-  return (
-    <div id="parent-overview">
-<nav className='nav'>
-  <h1>Nav bar will go here</h1>
-</nav>
-  <Gallery />
-<div className='overview-description'>
-  <h2>product description will go here</h2>
-</div>
 
+  return (
+<div id="parent-overview">
+  <nav className='nav'>
+    <h1>Nav bar will go here</h1>
+  </nav>
+  <Gallery />
+  <div className='overview-description'>
+    <h2>product description will go here</h2>
   </div>
-  )
+</div>
+)
 
 }
 
