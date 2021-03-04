29e683acbe765d4e9086123ddc80e54e7cb54f94
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-03 19:00:12 -0500

    Card #42: created Carousel!!

--- src/components/outfits/ProductCard.jsx
+++ src/components/outfits/ProductCard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Stars from '../shared/Stars.jsx';
 
 const ProductCard = ({product, index}) => {
-  console.log('this is the average rating: ', product.total);
   return (
     <div className="product-card" key={index}>
-      <img className="product-image" src={product.stylePhoto} alt={product.name} width="200" height="220"></img>
+      <img className="product-image" src={product.stylePhoto} alt={product.name} width="auto" height="220"></img>
       <span className="product-category">{product.category}</span>
       <h4 className="product-name">{product.name}</h4>
       <span className="product-price">${product.default_price}</span>
       {product.total &&
         <Stars className="product-rating" total={product.total}/>
+      }
+      {!product.total &&
+        <div>&nbsp;</div>
       }
     </div>
   )
--- src/components/outfits/index.js
+++ src/components/outfits/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Carousel from './Carousel.jsx';
 import CarouselContainer from '../../containers/outfits/CarouselContainer';
-import Comparison from './Comparison.jsx';
 
 const RelatedItems = ({fetchRelated, relatedProducts}) => {
 
@@ -13,7 +11,6 @@ const RelatedItems = ({fetchRelated, relatedProducts}) => {
     <div className="related-products">
       <h3 className="related-products-title">Related Products</h3>
       <CarouselContainer className="carousel" />
-      {/* <Comparison className="comparison-card" /> */}
       <h3 className="outfits-title">Your Outfit</h3>
       <CarouselContainer className="carousel" />
     </div>
