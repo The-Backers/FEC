728ecfa1e66fee74464a591c9965e229d11606dc
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-04 23:04:33 -0500

    working on card #44: got basic modal window to popup on star icon click

---
+++ src/components/outfits/ModalTest.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#app');
+
+const ModalTest = () => {
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+    return (
+      <div>
+        <button onClick={openModal} className="comparison-modal"></button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+      </div>
+    );
+}
+
+export default ModalTest;
--- src/components/outfits/ProductCard.jsx
+++ src/components/outfits/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stars from '../shared/Stars.jsx';
+import ModalTest from './ModalTest.jsx';
 
 const ProductCard = ({product, index, fetchProduct}) => {
 
@@ -7,20 +8,27 @@ const ProductCard = ({product, index, fetchProduct}) => {
     fetchProduct(id);
   }
 
+  const openComparison = () => {
+    console.log('testing comparison!');
+  }
+
   return (
-    <div className="product-card" key={index} onClick={() => handleClick(product.id)}>
+    <div className="product-card" key={index}>
       <div className="product-image-container">
-        <img className="product-image" src={product.stylePhoto} alt={product.name} width="auto" height="220"></img>
-        <i className="far fa-star"></i>
+        <img className="product-image" src={product.stylePhoto} alt={product.name} width="auto" height="220" onClick={() => handleClick(product.id)}></img>
+        <span onClick={openComparison.bind(this)}>
+          <i className="far fa-star"></i>
+          <ModalTest />
+        </span>
       </div>
-      <span className="product-category">{product.category}</span>
-      <h4 className="product-name">{product.name}</h4>
-      <span className="product-price">${product.default_price}</span>
+      <span className="product-category" onClick={() => handleClick(product.id)}>{product.category}</span>
+      <h4 className="product-name" onClick={() => handleClick(product.id)}>{product.name}</h4>
+      <span className="product-price" onClick={() => handleClick(product.id)}>${product.default_price}</span>
       {product.total &&
-        <Stars className="product-rating" total={product.total}/>
+        <Stars className="product-rating" total={product.total} onClick={() => handleClick(product.id)}/>
       }
       {!product.total &&
-        <div>&nbsp;</div>
+        <div onClick={() => handleClick(product.id)}>&nbsp;</div>
       }
     </div>
   )
