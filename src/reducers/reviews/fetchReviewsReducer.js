c5dc7cc9379293e367fed3f50d998a649f029c5d
David <dbdavidbacker@gmail.com>
2021-03-07 14:42:51 -0500

    Button disappears when there are less than two reviews total

--- src/reducers/reviews/fetchReviewsReducer.js
+++ src/reducers/reviews/fetchReviewsReducer.js
@@ -7,7 +7,17 @@ var fetchReviewsReducer = (state={}, action) => {
 
   if (action.type === 'SHOW_REVIEWS') {
 
+    if (Object.keys(state).length !== 0) {
 
+      console.log(action.reviews.results.length, state.results.length)
+      if (action.reviews.results.length < 2) {
+
+
+          action.reviews.done = 'none';
+
+
+      }
+    }
       action.reviews.sort = action.sort;
       return action.reviews || [];
 
