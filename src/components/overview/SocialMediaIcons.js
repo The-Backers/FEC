4e642b2d05b46d0c8b43975cbd82e5a6d5f63b2c
amenchaca100 <menchacanton@gmail.com>
2021-03-11 15:53:41 -0500

    updating master

--- src/components/overview/SocialMediaIcons.js
+++ src/components/overview/SocialMediaIcons.js
@@ -5,7 +5,7 @@ let SocialMediaIcons = () => (
   <div className='social-media-icons'>
 
   <div>SHARE</div>
-  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false"><div className='twitter'><i className="fab fa-twitter-square"></i></div></a><script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false"><div className='twitter'><i className="fab fa-twitter-square"></i></div></a><script src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
 
 
     <div className="fb-share-button" data-href={window.location} data-layout="button"  data-size="large"><a href={`https://www.facebook.com/sharer/sharer.php?u=${window.location}`} ><i className="fab fa-facebook-square"></i></a></div>
