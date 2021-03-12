b47658505eba3034071d873dd016fd845c02cf85
amenchaca100 <menchacanton@gmail.com>
2021-03-11 19:59:09 -0500

    accesibility 100

--- src/components/overview/ExpandedGallery.js
+++ src/components/overview/ExpandedGallery.js
@@ -12,7 +12,7 @@ let ExpandedGallery = ({index, collapseGallery, currentStyle}) => {
   return (
     <div  id='overview-body-expanded' >
       <div  id='overview-body-image-expanded'>
-        <img  onClick={() => imageZoom('overview-zoom-image-expanded', 2.5)} id='overview-zoom-image-expanded' src={currentStyle.photos[index].url} />
+        <img  width={500} height={600} onClick={() => imageZoom('overview-zoom-image-expanded', 2.5)} id='overview-zoom-image-expanded' src={currentStyle.photos[index].url} />
         <div id="myresult" className="img-zoom-result"></div>
         <a onClick={() => collapsedGallery()} ><i  className="fas fa-expand button-expander"></i></a>
         <div className='current-style-gallery'>
